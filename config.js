d07611e48a25115fd95f99b3d2b5897c9639b755
Alexon7 <alenkaalexon@gmail.com>
2023-08-18 14:13:43 +0200

    some change

--- config.js
+++ config.js
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC13WqPynQjN_JV29onYaXUMxkqw0JLjLI",
-  authDomain: "react-native-46cb5.firebaseapp.com",
-  projectId: "react-native-46cb5",
-  storageBucket: "react-native-46cb5.appspot.com",
-  messagingSenderId: "412549169001",
-  appId: "1:412549169001:web:4d6f91be4dd8b8c69907f8",
-  measurementId: "G-8BTMX6W6P5"
+  apiKey: "AIzaSyDIwLZ_xWhT3uIQRTJVDbohyzJYNPx1I-M",
+  authDomain: "study-8acc4.firebaseapp.com",
+  projectId: "study-8acc4",
+  storageBucket: "study-8acc4.appspot.com",
+  messagingSenderId: "167566869400",
+  appId: "1:167566869400:web:8d895a72d76099573a61ed",
+  measurementId: "G-VCQ54H9M0G"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -25,24 +25,3 @@ export const storage = getStorage(app);
 
 
 
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDIwLZ_xWhT3uIQRTJVDbohyzJYNPx1I-M",
-//   authDomain: "study-8acc4.firebaseapp.com",
-//   projectId: "study-8acc4",
-//   storageBucket: "study-8acc4.appspot.com",
-//   messagingSenderId: "167566869400",
-//   appId: "1:167566869400:web:32e3bc3b22dd13df3a61ed",
-//   measurementId: "G-TSHZQMHN8X"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
